427b9c58d74e2001e5e85901bbed4560c06a20da
ageuribeiro <ageu87@gmail.com>
2023-01-09 13:35:05 -0300

    New branch and include librares

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+import scipy
+import pyspark as spark
+import pyodbc as sql
+import pyhadoop as hadoop
+import docker
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
